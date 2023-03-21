cf470108b5c97dbfddb6cab659b692503544d6fe
André Chaves Teixeira <TRI0106062@aluno.firjansenaisesi.com.br>
2023-03-21 20:15:27 -0300

    stilos

--- src/home/style.tsx
+++ src/home/style.tsx
@@ -2,24 +2,3 @@ import styled from "styled-components";
 
 
 
-export const NewTask = styled.div`
-display: block;
-
-margin: 50px;
-
-
-`
-
-export const buttonAdd = styled.button`
-  background-color: #4CAF50; /* Green */
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-
-
-
-`
