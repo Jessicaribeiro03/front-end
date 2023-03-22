f973e8264709b1cd25aa1faa6ffd3cd11588a624
André Chaves Teixeira <TRI0106062@aluno.firjansenaisesi.com.br>
2023-03-21 21:14:57 -0300

    avançando no estilo

--- src/home/home-user-view.tsx
+++ src/home/home-user-view.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { NewTask } from "./style";
+import { NavTabs, NewTask, TarefasCriadas } from "./style";
 
 export class UserView extends React.Component{
     render(){
@@ -21,7 +21,7 @@ export class UserView extends React.Component{
                         </nav>
                         {/* NavBar Hamburger */}
                         {/* NavTabs */}
-                        <div className="container mt-5" >
+                        <NavTabs >
                         <ul className="nav nav-tabs" id="myTab" role="tablist">
                             <li className="nav-item" role="presentation">
                                 <button className="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Home</button>
@@ -38,22 +38,21 @@ export class UserView extends React.Component{
                             <div className="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
                             <div className="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
                             </div>
-                            </div>
+                            </NavTabs>
                              {/* NavTabs */}
                         {/* Formularios */}
                         {/* Adicionar Nova Tarefa */}
                                 <NewTask>
                                  
-                                     
-                                 <form className="d-flex justify-content-center align-items-center">
+                                    
+                                 
                                      <div className="form-outline flex-fill">
-                                        <input type="text" id="form2" className="form-control" />
-                                        <label className="form-label" htmlFor="form2">New task...</label>
+                                     <input className="form-control " type="text" placeholder="Nova Tarefa... " aria-label="default input example"/>
                                     </div>
-                                    <div className="position-sticky  ">
-                                        <button type="submit" id="buttonAdd" className="btn btn-info ">Add</button>
+                                    <div className="d-grid gap-2 col-1 mx-auto">
+                                        <button  className="btn btn-info btn-md mx-2 ">Add</button>
                                     </div>
-                                </form>
+                                
                                 
                                 
                                 </NewTask>
@@ -62,13 +61,15 @@ export class UserView extends React.Component{
 
                         {/* Adicionar Nova Tarefa */}
                         
-                        <div>
-                        <ul className="list-group p-5 justify-content-between">
-                                <li className="list-group-item d-flex flex-fill ">
-                                    <input className="form-check-input m-3" type="checkbox" value="" aria-label="..."/>
+                        <TarefasCriadas>
+                        <ul className="list-group ">
+                                <li className="list-group-item  ">
+                                    <input className="form-check-input " type="checkbox" value="" aria-label="..."/>
                                     <input className="form-control " type="text" placeholder="Default input" aria-label="default input example"/>
                                     {/* Icones */}
+                                    <div className="d-grid gap-1 col-1 mx-auto">
                                     <button>
+                                    
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-fill" viewBox="0 0 16 16">
                                     <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
                                     </svg>
@@ -79,11 +80,12 @@ export class UserView extends React.Component{
                                     <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
                                     </svg>
                                     </button>
+                                    </div>
                                     {/* Icones */}
                                    
                                 </li>
                               </ul>
-                              </div>
+                              </TarefasCriadas>
                                
                  </div>
         )
