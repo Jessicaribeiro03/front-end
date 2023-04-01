9891be88ab05e1654066bda8c359d4537acc7cb3
André Chaves Teixeira <TRI0106062@aluno.firjansenaisesi.com.br>
2023-03-31 21:26:06 -0300

    correçao PROF

--- src/login/models/service/login-service.tsx
+++ src/login/models/service/login-service.tsx
@@ -1,9 +1,8 @@
 export async function auth(authCode: string): Promise<any>{
-    const response = await fetch (`http://localhost:3010/auth/code/$(authCode)`, {
-
+    const response = await fetch (`http://localhost:3010/auth/code/${authCode}`, {
     method: 'GET',
     headers: {
-        "Acess-Control-Allow-Origin" : "*"
+        "Access-Control-Allow-Origin" : "*"
     }
        
 
--- src/login/views/controller/login-controller.tsx
+++ src/login/views/controller/login-controller.tsx
@@ -13,12 +13,12 @@ export default function LoginControler() {
         
 
     async function sendAuthCode(authCode: string) {
-       const response = await auth(authCode['acess_token']);
-       const tokenDecoded = jwtDecode (response['acess_token']); 
+       const response = await auth(authCode['access_token']);
+       const tokenDecoded = jwtDecode (response['token']); 
     
         Cookies.set(
             'acess_token',
-            response ['acess_token'],
+            response ['token'],
             {expires: 1}
         );
         if (tokenDecoded['birthday'] !== null){
